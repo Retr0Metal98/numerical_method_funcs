913b2ed8fdfe5889232e5cac7f725d58c340d706
Retr0Metal98 <66635436+Retr0Metal98@users.noreply.github.com>
2021-11-21 23:58:35 +0800

    added other convergence criteria for iterated Ax=b algos

--- gauss_seidel.m
+++ gauss_seidel.m
@@ -1,4 +1,40 @@
-function [x1, flag, enorm, err_list, err_count] = gauss_seidel(A,b,x0,num_iter)
+function [x1, flag, enorm] = gauss_seidel(A,b,x0,num_iter)
+% Gauss-Seidel iterative algorithm to solve Ax=b
+% for square matrices A
+[m,n] = size(A);
+Dinv = zeros(m,n);
+C = A;
+for i=1:n
+    Dinv(i,i) = 1/A(i,i);
+    C(i,i) = 0;
+end
+
+flag = 0; % whether solution is found
+zero_threshold = 1e-10;
+x = x0;
+[x_m, x_n] = size(x);
+for i=1:num_iter
+    for j=1:x_m
+        x(j) = Dinv(j,j)*(b(j)-C(j,:)*x);
+    end
+    e = b - A*x; % length of error vector
+    enorm = (e'*e)^0.5; % or sqrt(sum(e.^2));
+    
+    % normalised length of error vector (divide by length of b):
+    % enorm = ((e'*e)^0.5)/((b'*b)^0.5); % or sqrt(sum(e.^2))/sqrt(sum(b.^2));
+    % length of projection vector:
+    % dx = x1 - x0;
+    % enorm = (dx'*dx)^0.5; % or sqrt(sum(dx.^2));
+    if enorm < zero_threshold
+        flag = 1;
+        break;
+    end
+end
+x1 = x;
+end
+
+
+function [x1, flag, enorm, err_list, err_count] = gauss_seidel_return_errs(A,b,x0,num_iter)
 % Gauss-Seidel iterative algorithm to solve Ax=b
 % for square matrices A
 [m,n] = size(A);
--- jacobi.m
+++ jacobi.m
@@ -1,4 +1,35 @@
-function [x1, flag, enorm, err_list, err_count] = jacobi(A,b,x0,num_iter)
+function [x1, flag, enorm] = jacobi(A,b,x0,num_iter)
+% Jacobi iterative algorithm to solve Ax=b
+% for square matrices A
+[m,n] = size(A);
+Dinv = zeros(m,n);
+C = A;
+for i=1:n
+    Dinv(i,i) = 1/A(i,i);
+    C(i,i) = 0;
+end
+
+flag = 0; % whether solution is found
+zero_threshold = 1e-10;
+for i=1:num_iter
+    x1 = Dinv*(b-C*x0); % new x
+    e = b - A*x1; % length of error vector
+    enorm = (e'*e)^0.5; % or sqrt(sum(e.^2));
+    
+    % normalised length of error vector (divide by length of b):
+    % enorm = ((e'*e)^0.5)/((b'*b)^0.5); % or sqrt(sum(e.^2))/sqrt(sum(b.^2));
+    % length of projection vector:
+    % dx = x1 - x0;
+    % enorm = (dx'*dx)^0.5; or sqrt(sum(dx.^2));
+    if enorm < zero_threshold 
+        flag = 1;
+        break;
+    end
+    x0 = x1; % update old x to new x
+end
+end
+
+function [x1, flag, enorm, err_list, err_count] = jacobi_return_errs(A,b,x0,num_iter)
 % Jacobi iterative algorithm to solve Ax=b
 % for square matrices A
 [m,n] = size(A);
@@ -19,7 +50,7 @@ for i=1:num_iter
     enorm = (e'*e)^0.5;
     err_count = err_count + 1;
     err_list(err_count,1) = enorm;
-    if enorm < zero_threshold
+    if enorm < zero_threshold % length of error vector
         flag = 1;
         break;
     end
